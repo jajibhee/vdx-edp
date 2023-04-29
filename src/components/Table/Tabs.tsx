53e5782864f1613ee422291a5811fff5552597f9
jaji oluwatobiloba <jajmantobi@gmail.com>
2023-04-29 02:04:50 -0400

    Fix import error

--- src/components/Table/Tabs.tsx
+++ src/components/Table/Tabs.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
+
 import Box from '@mui/material/Box';
-import TableTabs from './TableTabs';
-import DataTable from './EdpTable';
+
 import AttendeesTable from './AttendeesTable';
 import SponsorsTable from './SponsorsTable';
 
